e5d394df5639968ecf7e8519dd471218e902ea18
Annette <annette.ngugi@student.moringaschool.com>
2025-04-25 19:23:16 +0300

    updated

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,17 +1,20 @@
+import React from "react";
 import Article from "./Article";
 
-function ArticleList({posts}) {
-    return(
-        <main>
-       {posts.map((post)=>{
-      <Article title={post.title}
-               date={post.date}
-               preview={post.preview}
-               key={post.id}
-      />
-       })}
-        </main>
-    )
+function ArticleList({ posts }) {
+  return (
+    <main>
+      {posts.map((post) => (
+        <Article
+          key={post.id}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
+          minutes={post.minutes}
+        />
+      ))}
+    </main>
+  );
 }
 
 export default ArticleList;
